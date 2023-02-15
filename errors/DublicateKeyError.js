9bb3e19ed7c03d21332e499b9ee9405dc7974e6b
IWToIT <100576565+IWToIT@users.noreply.github.com>
2023-02-15 13:29:29 +0300

    Delete errors directory

--- errors/DublicateKeyError.js
+++
@@ -1,9 +0,0 @@
-class DublicateKeyError extends Error {
-  constructor(message) {
-    super(message);
-    this.errorMessage = message;
-    this.statusCode = 409;
-  }
-}
-
-module.exports = DublicateKeyError;
